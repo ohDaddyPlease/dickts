7163aeb785bef2df1d1a6c91933dee643cd741ff
Sergey Novikov <si.novikov@vk.team>
2023-01-22 18:23:24 +0300

    change yaml struct

--- config/db_config.go
+++ config/db_config.go
@@ -1,9 +1,9 @@
 package config
 
 type DB struct {
-	Port         string
-	Address      string
-	UserName     string
-	Password     string
-	DatabaseName string
+	Port         string `yaml:"port"`
+	Address      string `yaml:"address"`
+	UserName     string `yaml:"user_name"`
+	Password     string `yaml:"password"`
+	DatabaseName string `yaml:"database_name"`
 }
